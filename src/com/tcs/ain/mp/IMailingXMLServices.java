2bd85b0e6dcfe192eaedbe9e0eb0e3515e7d670d
PADMAVATHI KILLI <71591291+padma-killi@users.noreply.github.com>
2023-04-24 18:20:00 +0530

    Update IMailingXMLServices.java

--- src/com/tcs/ain/mp/IMailingXMLServices.java
+++ src/com/tcs/ain/mp/IMailingXMLServices.java
@@ -3,25 +3,6 @@ package com.tcs.ain.mp;
 import com.tcs.ain.mp.common.MailingException;
 import com.tcs.ain.mp.common.FatalException;
 
-/* $Revision: 1.4 $
-* Copyright � 2003  Global Net Services Inc
-* All Rights Reserved
-*
-* This is unpublished proprietary source code.
-* The copyright notice above does not evidence any actual or
-* intended publication of such source code.
-*/
-
-/**
- * IMailingXMLServices - A common interface for all the mailing types
- * 
- * @version 1.00 25 th October
- * @author Christy Mathew
- * 
- * @see MailingException
- * @see FatalException
- */
-
 public interface IMailingXMLServices 
 {
    /**
