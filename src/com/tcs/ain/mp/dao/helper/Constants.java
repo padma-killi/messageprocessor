57cd1e30d93176d36a76e9e8eae691f0cb52f582
PADMAVATHI KILLI <71591291+padma-killi@users.noreply.github.com>
2023-04-24 18:11:30 +0530

    Update Constants.java

--- src/com/tcs/ain/mp/dao/helper/Constants.java
+++ src/com/tcs/ain/mp/dao/helper/Constants.java
@@ -1,8 +1,6 @@
 package com.tcs.ain.mp.dao.helper;
 
 /* $Revision: 1.1 $
- * Copyright � 2003  Global Net Services Inc
- * All Rights Reserved
  *
  * This is unpublished proprietary source code.
  * The copyright notice above does not evidence any actual or
